196e9b6bdfa126ce555df3d899e4b2f2d8c8442a
中西健治 <nakanishi@framelunch.jp>
2015-03-06 16:14:00 +0900

    dev1

--- public/api/deploy/index.php
+++ public/api/deploy/index.php
@@ -1,12 +1,15 @@
 <?php
 $LOG_FILE = __DIR__.'/hook.log';
-
+echo "start\n<pre>";
+var_dump($_POST);
 if (isset($_POST['payload']) ) {
     $payload = json_decode($_POST['payload'], true);
-    if ($payload['ref'] === 'refs/heads/develop') {
-        exec(`cd /var/www/sample; git pull origin develop:local`);
+    if ($_POST['payload'] === 'refs/heads/develop') {
+        echo "execute : \n";
+        exec(`cd /var/www/lamp/sample; git pull origin develop:local;`);
         file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['head_commit']['message']."\n", FILE_APPEND|LOCK_EX);
     }
 } else {
     file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 }
+echo "</pre>end\n";
