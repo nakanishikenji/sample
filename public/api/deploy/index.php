e966247db5baf72364db0b3d16c30a825ff0a638
中西健治 <nakanishi@framelunch.jp>
2015-03-06 16:22:07 +0900

    dev3

--- public/api/deploy/index.php
+++ public/api/deploy/index.php
@@ -6,7 +6,8 @@ if (isset($_POST['payload']) ) {
     $payload = json_decode($_POST['payload'], true);
     if ($_POST['payload'] === 'refs/heads/develop') {
         echo "execute : \n";
-        exec(`cd /var/www/lamp/sample; git pull origin develop:local;`);
+        exec(`cd /var/www/lamp/sample; git pull origin develop:local 2>&1`, $array);
+        var_dump($array);
         file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['head_commit']['message']."\n", FILE_APPEND|LOCK_EX);
     }
 } else {
