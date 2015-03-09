fc4bc612a6523e50be29905947b903bc499f103a
中西健治 <nakanishi@framelunch.jp>
2015-03-09 14:04:15 +0900

    dev

--- public/api/deploy/index.php
+++ public/api/deploy/index.php
@@ -20,7 +20,7 @@ $REMOTE_ADDR = $_SERVER['REMOTE_ADDR'];
 $access_key = getallheaders()[ACCESSKEY];
 $postdata = file_get_contents("php://input");
 
-echo '13';
+echo '14';
 echo 'hash' . "\n";
 echo hash_hmac(ALG, $postdata,SECRETKEY);
 
